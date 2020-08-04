3dccabea784ed071814d6ad8864361a92fe7d241
babuj03 <babuj03@gmail.com>
2020-08-05 00:18:37 +0100

    Update ServiceUtil.java

--- src/main/java/com/user/info/util/ServiceUtil.java
+++ src/main/java/com/user/info/util/ServiceUtil.java
@@ -1,43 +1,39 @@
 package com.user.info.util;
 
-
-
 import net.sf.geographiclib.Geodesic;
 import net.sf.geographiclib.GeodesicData;
 import net.sf.geographiclib.GeodesicMask;
 
 public class ServiceUtil {
 
-	public static String SPRING_PROFILE_DEVELOPMENT = "dev";
-    public static String DEFAULT_DISTANCE_UNIT =  "MILE";
-    
-	
-	public static double distance(double cityLatitude, double cityLongtitude, double userLat, double userLon, String unit) {
-		GeodesicData g = Geodesic.WGS84.Inverse(cityLatitude, cityLongtitude, userLat,
-				userLon,
-                GeodesicMask.DISTANCE);
-		
-		if(unit.equalsIgnoreCase("km") || unit.equalsIgnoreCase("kilometer"))
-		  return g.s12/1000;
-		
-		if(unit.equalsIgnoreCase("meter"))
-			 return g.s12;
-		
-		//Default convert into Mile
-		 return g.s12/1609;
+	public static final int CONVERT_METER_TO_MILES = 1609;
+
+	public static final int CONVERT_METER_TO_KM = 1000;
+
+	public static String KM_UNIT = "KM";
+
+	public static String METER_UNIT = "METER";
+
+	public static double distance(double cityLatitude, double cityLongtitude, double userLat, double userLon,
+			String unit) {
+		GeodesicData g = Geodesic.WGS84.Inverse(cityLatitude, cityLongtitude, userLat, userLon, GeodesicMask.DISTANCE);
+
+		if (unit.equalsIgnoreCase(KM_UNIT))
+			return g.s12 / CONVERT_METER_TO_KM;
+
+		if (unit.equalsIgnoreCase(METER_UNIT))
+			return g.s12;
+
+		// Default convert into Mile
+		return g.s12 / CONVERT_METER_TO_MILES;
 	}
-	
-	
-    public static boolean isStringOnlyAlphabet(String str) 
-    { 
-        return ((str != null) 
-                && (!str.equals("")) 
-                && (str.matches("^[a-zA-Z]*$"))); 
-    } 
-    
-    
-    public static String capitalize(final String city) {
-	return Character.toUpperCase(city.charAt(0)) + city.substring(1).toLowerCase();
-    }
-	
+
+	public static boolean isStringOnlyAlphabet(String str) {
+		return ((str != null) && (!str.equals("")) && (str.matches("^[a-zA-Z]*$")));
+	}
+
+	public static String capitalize(final String city) {
+		return Character.toUpperCase(city.charAt(0)) + city.substring(1).toLowerCase();
+	}
+
 }
