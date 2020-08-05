140a13b9f57a47fa4671e0b51f253e62711b1610
babuj03 <babuj03@gmail.com>
2020-08-06 00:05:50 +0100

    removed redundent annotation

--- src/main/java/com/user/info/UsersApp.java
+++ src/main/java/com/user/info/UsersApp.java
@@ -14,7 +14,6 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 @EnableFeignClients
-@ComponentScan
 public class UsersApp {
 
     private static final Logger log = LoggerFactory.getLogger(UsersApp.class);
